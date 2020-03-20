1a516ca05b7fc45463010b2fe040613e6a52d21b
Manuel Karakaya <karakaya.manuel@gmail.com>
2020-03-20 21:37:07 +0100

    Init commit

---
+++ src/main/java/com/Byteschupser/Erntehelfer/ErntehelferApplication.java
@@ -0,0 +1,13 @@
+package com.Byteschupser.Erntehelfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErntehelferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErntehelferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Byteschupser/Erntehelfer/ErntehelferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Byteschupser.Erntehelfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErntehelferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
