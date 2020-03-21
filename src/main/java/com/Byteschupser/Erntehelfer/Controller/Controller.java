f6b56c63a2a9083ebdaf9ae043990adb24effb57
Bomber0008 <bomber.0008@yahoo.com>
2020-03-21 11:01:16 +0100

    Initialized Controller

--- src/main/java/com/Byteschupser/Erntehelfer/Controller/Controller.java
+++ src/main/java/com/Byteschupser/Erntehelfer/Controller/Controller.java
@@ -1,7 +1,19 @@
 package com.Byteschupser.Erntehelfer.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @PostMapping(value = "/anzeige/erstellen")
+    public void neueAnzeigeErstellen() {
+
+    }
+
+    @GetMapping(value = "/anzeige/holen")
+    public void anzeigeAusDatenbankHolen(){
+
+    }
 }
